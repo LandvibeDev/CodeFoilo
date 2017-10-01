3944b542720abdc3b8789d27430acf937dc005a7
KeonHee <kimkh6554@gmail.com>
2017-10-01 21:48:05 +0900

    Add comments at Corsfilter.java

--- server/src/main/java/com/landvibe/codefolio/config/security/CorsFilter.java
+++ server/src/main/java/com/landvibe/codefolio/config/security/CorsFilter.java
@@ -9,15 +9,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+/**
+ * CORS filter에 대한 정의
+ *
+ */
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
+    /**
+     * 모든 Request에 대한 Response에 아래와 같은 헤더값을 추가한다
+     */
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-auth-token, content-type");
